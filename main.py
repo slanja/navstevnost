00118806d827c100770907effc6920671283b62f
slany3.jan <97552526+Jan-Slany@users.noreply.github.com>
2022-07-04 14:04:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
 
 import time
 import csv
